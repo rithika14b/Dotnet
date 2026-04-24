b31f89689b219987da6da2f229679825f1ed6c7b
Rithika Balasubramani <rithikab@infinite.com>
2026-04-24 20:04:58 +0530

    sql assignment

---
+++ Assignment/SQL/Assignment2/SQLQuery4.sql
@@ -0,0 +1,112 @@
+create database Assignment2
+use Assignment2
+
+CREATE TABLE DEPT(
+    DEPTNO INT PRIMARY KEY,
+    DNAME VARCHAR(20),
+    LOC VARCHAR(20)
+);
+
+INSERT INTO DEPT VALUES 
+(10, 'ACCOUNTING', 'NEW YORK'),
+(20, 'RESEARCH', 'DALLAS'),
+(30, 'SALES', 'CHICAGO'),
+(40, 'OPERATIONS', 'BOSTON');
+
+SELECT * FROM DEPT;
+
+CREATE TABLE EMP (
+    EMPNO INT PRIMARY KEY,
+    ENAME VARCHAR(20),
+    JOB VARCHAR(20),
+    MGR_ID INT NULL,
+    HIREDATE DATE,
+    SAL INT,
+    COMM INT NULL,
+    DEPTNO INT,
+    FOREIGN KEY (DEPTNO) REFERENCES DEPT(DEPTNO)
+);
+INSERT INTO EMP VALUES
+(7369, 'SMITH', 'CLERK', 7902, '1980-12-17', 800, NULL, 20),
+(7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', 1600, 300, 30),
+(7521, 'WARD', 'SALESMAN', 7698, '1981-02-22', 1250, 500, 30),
+(7566, 'JONES', 'MANAGER', 7839, '1981-04-02', 2975, NULL, 20),
+(7654, 'MARTIN', 'SALESMAN', 7698, '1981-09-28', 1250, 1400, 30),
+(7698, 'BLAKE', 'MANAGER', 7839, '1981-05-01', 2850, NULL, 30),
+(7782, 'CLARK', 'MANAGER', 7839, '1981-06-09', 2450, NULL, 10),
+(7788, 'SCOTT', 'ANALYST', 7566, '1987-04-19', 3000, NULL, 20),
+(7839, 'KING', 'PRESIDENT', NULL, '1981-11-17', 5000, NULL, 10),
+(7844, 'TURNER', 'SALESMAN', 7698, '1981-09-08', 1500, 0, 30),
+(7876, 'ADAMS', 'CLERK', 7788, '1987-05-23', 1100, NULL, 20),
+(7900, 'JAMES', 'CLERK', 7698, '1981-12-03', 950, NULL, 30),
+(7902, 'FORD', 'ANALYST', 7566, '1981-12-03', 3000, NULL, 20),
+(7934, 'MILLER', 'CLERK', 7782, '1982-01-23', 1300, NULL, 10);
+
+SELECT * FROM EMP;
+
+-- 1)
+
+SELECT * FROM EMP WHERE ename LIKE 'A%';
+
+-- 2)
+
+SELECT * FROM EMP WHERE MGR_ID IS NULL;
+
+--3)
+
+SELECT ename, empno, sal FROM EMP
+WHERE sal BETWEEN 1200 AND 1400;
+
+--4)
+
+SELECT e.* FROM EMP e JOIN DEPT d ON e.deptno = d.deptno
+WHERE d.dname = 'RESEARCH';
+
+--5)
+
+SELECT COUNT(*) AS "Number of Clerks" FROM EMP
+WHERE job = 'CLERK';
+
+--6)
+
+SELECT job,AVG(sal) AS avg_salary,COUNT(*) AS total_employees
+FROM EMP GROUP BY job;
+
+--7)
+
+SELECT * FROM EMP
+WHERE sal = (SELECT MAX(sal) FROM EMP)OR sal = (SELECT MIN(sal) FROM EMP);
+
+--8)
+
+SELECT d.* FROM DEPT d LEFT JOIN EMP e ON d.deptno = e.deptno
+WHERE e.empno IS NULL;
+
+--9)
+
+SELECT ename, sal FROM EMP WHERE job = 'ANALYST'AND sal > 1200 AND deptno = 20
+ORDER BY ename ASC;
+
+--10)
+
+SELECT d.dname,d.deptno, SUM(e.sal) AS total_salary FROM DEPT d
+JOIN EMP e ON d.deptno = e.deptno GROUP BY d.dname, d.deptno;
+
+--11)
+
+SELECT ename, sal FROM EMP WHERE ename IN ('MILLER', 'SMITH');
+
+--12) 
+
+SELECT ename FROM EMP WHERE ename LIKE 'A%'OR ename LIKE 'M%';
+
+--13) 
+
+SELECT ename, sal * 12 AS yearly_salary FROM EMP WHERE ename = 'SMITH';
+
+--14)
+SELECT ename, sal FROM EMP WHERE sal NOT BETWEEN 1500 AND 2850;
+
+--15) 
+
+SELECT mgr_id, COUNT(*) AS num_reports FROM EMP WHERE mgr_id IS NOT NULL GROUP BY mgr_id HAVING COUNT(*) > 2;
